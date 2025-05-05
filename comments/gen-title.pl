d621ce25ee71800cee4ecc13cdcd49194fb1df4f
Carlos Maniero <carlos@maniero.me>
2025-05-04 22:30:47 -0300

    ci: automatically build the site
    Signed-off-by: Carlos Maniero <carlos@maniero.me>

--- comments/gen-title.pl
+++ comments/gen-title.pl
@@ -3,7 +3,7 @@ my $filename = $ARGV[0];
 open(my $fh, "<", $filename) || die "Can't open file: $!"; # Open the file in read mode
 
 while (<$fh>) {
-  if (m/<h2 class="chapter">\d+ (.*)<\/h2>/) {
+  if (m/<h2 class="chapter".*>\d+ (.*?)(<a.*>)*<\/h2>/) {
     close $fh;
 
     open(my $fht,'>', $filename . ".title") or die $!;
